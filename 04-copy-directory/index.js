e6ccb83fd389e392b6b2937e6607bc7ae94657df
Vika Lubenets <lubenetcvika@gmail.com>
2023-05-06 21:19:03 +0400

    feat: add solution for 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,48 @@
+const path = require('path');
+const fs = require('fs');
 
+const pathFolderCurr = path.resolve(__dirname, 'files');
+const pathFolderTarget = path.resolve(__dirname, 'files-copy');
+
+function copyDir(source, target) {
+  fs.readdir(source, { withFileTypes: true }, (err, files) => {
+    if (err) {
+      return console.log(`Error reading directory: ${err}`);
+    }
+    files.forEach((file) => {
+      const pathSource = path.join(source, file.name);
+      const pathTarget = path.join(target, file.name);
+      if (file.isDirectory()) {
+        fs.mkdir(pathTarget, { recursive: true }, (err) => {
+          if (err) {
+            console.log(`Error creating directory: ${err}`);
+          } else {
+            console.log(`Directory created: ${pathTarget}`);
+            copyDir(pathSource, pathTarget);
+          }
+        });
+      } else if (file.isFile()) {
+        fs.copyFile(pathSource, pathTarget, (err) => {
+          if (err) throw err;
+          console.log(`file ${file.name} was copied to files-copy directory`);
+        });
+      }
+    });
+  });
+}
+
+fs.stat(pathFolderTarget, (err, stats) => {
+  if (err) {
+    fs.mkdir(pathFolderTarget, { recursive: true }, (err) => {
+      if (err) {
+        console.log(`Error creating directory: ${err}`);
+      } else {
+        console.log('Folder created successfully!');
+        copyDir(pathFolderCurr, pathFolderTarget);
+      }
+    });
+  } else {
+    console.log('Directory already exists');
+    copyDir(pathFolderCurr, pathFolderTarget);
+  }
+});
