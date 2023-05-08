fbaf57f075068e2ae09a2aa8a5bcca40d219f112
Vika Lubenets <lubenetcvika@gmail.com>
2023-05-08 15:24:41 +0400

    feat: add solution for 06-build-page

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,23 +1,22 @@
 const path = require('path');
 const fs = require('fs');
-const process = require('process');
 
 // Создаёт папку  **project-dist**
 const pathFolderTarget = path.resolve(__dirname, 'project-dist');
 
 fs.stat(pathFolderTarget, (err, stats) => {
-    if (err) {
-      fs.mkdir(pathFolderTarget, { recursive: true }, (err) => {
-        if (err) {
-          console.log(`Error creating directory: ${err}`);
-        } else {
-          console.log('Folder created successfully!');
-        }
-      });
-    } else {
-      console.log('Directory already exists');
-    }
-  });
+  if (err) {
+    fs.mkdir(pathFolderTarget, { recursive: true }, (err) => {
+      if (err) {
+        console.log(`Error creating directory: ${err}`);
+      } else {
+        console.log('Folder project-dist created successfully!');
+      }
+    });
+  } else {
+    console.log('Directory project-dist already exists');
+  }
+});
 
 // Собирает в единый файл стили из папки **styles** и помещает их в файл **project-dist/style.css**.
 
@@ -26,7 +25,7 @@ const pathFolder = path.resolve(__dirname, 'styles');
 fs.readdir(pathFolder, (err, files) => {
   const arr = [];
   if (err) {
-    return console.log(`Error reading directory: ${err}`);
+    return console.log(`Error reading directory styles: ${err}`);
   }
   for (const file of files) {
     const ext = path.extname(file);
@@ -39,8 +38,8 @@ fs.readdir(pathFolder, (err, files) => {
 
   const pathStyle = path.resolve(__dirname, 'project-dist/style.css');
   const writeStream = fs.createWriteStream(pathStyle, { flags: 'a', encoding: 'utf-8' });
-  writeStream.on('error', (err) => console.log(`Error writing to file: ${err}`));
-  writeStream.on('finish', () => console.log('Data written to file'));
+  writeStream.on('error', (err) => console.log(`Error writing to file style: ${err}`));
+  writeStream.on('finish', () => console.log('Data written to file style'));
   arr.forEach((stream) => stream.pipe(writeStream));
 });
 
@@ -69,7 +68,7 @@ function copyDir(source, target) {
       } else if (file.isFile()) {
         fs.copyFile(pathSource, pathTarget, (err) => {
           if (err) throw err;
-          console.log(`file ${file.name} was copied to files-copy directory`);
+          console.log(`file ${file.name} was copied to assets directory`);
         });
       }
     });
@@ -82,12 +81,53 @@ fs.stat(pathFolderCopy, (err, stats) => {
       if (err) {
         console.log(`Error creating directory: ${err}`);
       } else {
-        console.log('Folder created successfully!');
+        console.log('Folder project-dist/assets created successfully!');
         copyDir(pathFolderAssets, pathFolderCopy);
       }
     });
   } else {
-    console.log('Directory already exists');
+    console.log('Directory project-dist/assets already exists');
     copyDir(pathFolderAssets, pathFolderCopy);
   }
 });
+
+// Замена тэгов в файле **template.html** на содержимое одноимённых компонентов
+
+const sourceFolder = path.resolve(__dirname, 'components');
+const targetFolder = path.resolve(__dirname, 'project-dist');
+const targetFile = path.resolve(__dirname, 'project-dist/index.html');
+const templateFile = path.resolve(__dirname, 'template.html');
+
+fs.stat(targetFile, (err, stats) => {
+  if (err) {
+    console.log('File index.html already exists!');
+  } else {
+    console.log('File index.html created successfully');
+    copyDir(templateFile, targetFile);
+  }
+});
+
+async function buildPage() {
+  try {
+    const files = await fs.promises.readdir(sourceFolder, { withFileTypes: true });
+    let template = await fs.promises.readFile(templateFile, 'utf-8');
+    for (const file of files) {
+      const ext = path.extname(file.name);
+      if (ext === '.html') {
+        const pathSource = path.join(sourceFolder, file.name);
+        const componentName = path.parse(file.name).name;
+        const component = await fs.promises.readFile(pathSource, 'utf-8');
+        template = template.split(`{{${componentName}}}`).join(component);
+      } else {
+        console.log(`Skipping file with extension ${ext}`);
+      }
+    }
+    await fs.promises.mkdir(targetFolder, { recursive: true });
+    await fs.promises.writeFile(targetFile, template, 'utf-8');
+    console.log('index.html created successfully');
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+buildPage();
